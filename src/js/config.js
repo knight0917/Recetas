e63617ea160eefba1cc2b81a18b018eb74795eca
knightmoon <ankitakshay78@gmail.com>
2022-07-02 18:09:49 +0530

    update nd done

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '951dbaba-2248-4f10-beb5-1855ab2ee7f1';
+export const MODAL_CLOSE_SEC = 2.5;
